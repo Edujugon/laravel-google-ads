8cf680636263914ebfca08abba845a31f9a4bc5b
Eduardo Marcos <edujugon@hotmail.com>
2017-02-12 12:58:52 +0100

    Refactoring Now you can instance Service class directly and pass the service type throw the service method.

--- src/Services/Service.php
+++ src/Services/Service.php
@@ -16,16 +16,34 @@ use Google\AdsApi\AdWords\AdWordsServices;
 class Service
 {
 
+    /**
+     * @var string
+     */
     protected $postQuery = '';
 
+    /**
+     * @var mixed
+     */
     protected $session;
 
+    /**
+     * @var AdWordsServices
+     */
     protected $adWordsServices;
 
+    /**
+     * @var
+     */
     protected $service;
 
+    /**
+     * @var
+     */
     protected $fields;
 
+    /**
+     * Service constructor.
+     */
     function __construct()
     {
         $this->adWordsServices = new AdWordsServices();
@@ -33,20 +51,35 @@ class Service
         $this->session = (new AdwordsSession())->oAuth()->build();
     }
 
+    /**
+     * @param $field
+     * @return $this
+     */
     public function orderBy($field)
     {
-        $this->postQuery = "ORDER BY $field";
+        $this->postQuery .= " ORDER BY $field";
 
         return $this;
     }
 
-    public function limit($number,$offset = 0)
+    /**
+     * @param $number
+     * @param int $offset
+     * @return $this
+     */
+    public function limit($number, $offset = 0)
     {
         $this->postQuery .= " LIMIT $offset,$number";
 
         return $this;
     }
 
+    /**
+     * Get all items.
+     *
+     * @param array $fields
+     * @return mixed
+     */
     public function all($fields = [])
     {
         $fields = empty($fields) ? $this->fields : $fields;
@@ -55,6 +88,16 @@ class Service
         $query = 'SELECT '. implode(',',$fields) . $this->postQuery;
 
         return $this->service->query($query);
+    }
+
+    /**
+     * Set the service
+     *
+     * @param $service
+     */
+    public function service($service)
+    {
+        $this->service = $service;
     }
 
 }
