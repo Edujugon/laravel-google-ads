a07f350b64dcbaeba0617880f70d4a724e9514e7
Eduardo Marcos <edujugon@hotmail.com>
2017-08-21 13:29:42 +0200

    Update google version for reports

--- src/Reports/Report.php
+++ src/Reports/Report.php
@@ -11,8 +11,8 @@ namespace Edujugon\GoogleAds\Reports;
 
 
 use Edujugon\GoogleAds\Session\AdwordsSession;
-use Google\AdsApi\AdWords\Reporting\v201702\ReportDownloader;
-use Google\AdsApi\AdWords\v201702\cm\ApiException;
+use Google\AdsApi\AdWords\Reporting\v201708\ReportDownloader;
+use Google\AdsApi\AdWords\v201708\cm\ApiException;
 
 class Report
 {
